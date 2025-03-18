87af8d9b67a4546dd3d72d7a1eb9a3d0a2b08544
Xist <xist@xist.gg>
2025-03-17 19:47:55 -0700

    Very simple update to ABP_Mass, when the simulation is paused, play Idle animation, otherwise walk.

---
+++ Source/MassTimeGame/Private/MTGBlueprintHelpers.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2025 Xist.GG
+
+#include "MTGBlueprintHelpers.h"
+
+#include "MassSimulationSubsystem.h"
+#include "Engine/World.h"
+
+bool UMTGBlueprintHelpers::IsSimulationPaused(const UObject* WorldContextObject)
+{
+	bool bIsPaused {false};
+	if (WorldContextObject)
+	{
+		if (const UMassSimulationSubsystem* MassSimulationSubsystem = UWorld::GetSubsystem<UMassSimulationSubsystem>(WorldContextObject->GetWorld()))
+		{
+			bIsPaused = MassSimulationSubsystem->IsSimulationPaused();
+		}
+	}
+	return bIsPaused;
+}
---
+++ Source/MassTimeGame/Public/MTGBlueprintHelpers.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2025 Xist.GG
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MTGBlueprintHelpers.generated.h"
+
+class UMTGSimTimeSubsystem;
+
+UCLASS(meta=(BlueprintThreadSafe, DisplayName="MTG Blueprint Helpers"))
+class MASSTIMEGAME_API UMTGBlueprintHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Is the Mass Simulation currently Paused?
+	 * @param WorldContextObject Any world object so we can get the Mass Simulation Subsystem
+	 * @return True if the simulation is currently paused, else False
+	 */
+	UFUNCTION(BlueprintPure, Category="MassTimeGame", meta=(WorldContext="WorldContextObject"))
+	static bool IsSimulationPaused(const UObject* WorldContextObject);
+};
